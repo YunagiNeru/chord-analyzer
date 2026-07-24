8eb6781ff6e1b95d0f0925f84f8439aa688a9834
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-24 23:02:10 +0900

    Install bounded resolver recovery after finalization

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,10 +1,12 @@
 from .agents import MusicCoordinatorAgent
 from .accuracy_v2_finalization_v2 import install_accuracy_v2_finalization
 from .accuracy_v2_release_recovery import install_accuracy_v2_release_recovery
+from .accuracy_v2_resolver_recovery import install_accuracy_v2_resolver_recovery
 from .quality_extension import install_quality_extension
 
 install_quality_extension()
 install_accuracy_v2_release_recovery()
 install_accuracy_v2_finalization()
+install_accuracy_v2_resolver_recovery()
 
 __all__ = ["MusicCoordinatorAgent"]
