c4650c96f445acde2351af63a20033608c252fa5
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-25 01:08:52 +0900

    Install production release mode after resolver recovery

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,5 +1,6 @@
 from .agents import MusicCoordinatorAgent
 from .accuracy_v2_finalization_v2 import install_accuracy_v2_finalization
+from .accuracy_v2_release_mode import install_accuracy_v2_release_mode
 from .accuracy_v2_release_recovery import install_accuracy_v2_release_recovery
 from .accuracy_v2_resolver_gate import install_accuracy_v2_resolver_gate
 from .accuracy_v2_resolver_recovery import install_accuracy_v2_resolver_recovery
@@ -10,5 +11,6 @@ install_accuracy_v2_release_recovery()
 install_accuracy_v2_finalization()
 install_accuracy_v2_resolver_recovery()
 install_accuracy_v2_resolver_gate()
+install_accuracy_v2_release_mode()
 
 __all__ = ["MusicCoordinatorAgent"]
