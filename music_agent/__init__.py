7032ca56d94bb1c8fd2803d795f055a4f23a12f8
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-24 23:05:41 +0900

    Install conditional resolver failure gate

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,6 +1,7 @@
 from .agents import MusicCoordinatorAgent
 from .accuracy_v2_finalization_v2 import install_accuracy_v2_finalization
 from .accuracy_v2_release_recovery import install_accuracy_v2_release_recovery
+from .accuracy_v2_resolver_gate import install_accuracy_v2_resolver_gate
 from .accuracy_v2_resolver_recovery import install_accuracy_v2_resolver_recovery
 from .quality_extension import install_quality_extension
 
@@ -8,5 +9,6 @@ install_quality_extension()
 install_accuracy_v2_release_recovery()
 install_accuracy_v2_finalization()
 install_accuracy_v2_resolver_recovery()
+install_accuracy_v2_resolver_gate()
 
 __all__ = ["MusicCoordinatorAgent"]
