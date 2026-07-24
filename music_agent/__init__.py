1ee36bf5a266900609151460b6d42ccdbb9bce76
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-24 14:12:42 +0900

    Remove pipeline monkey patch

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,7 +1,3 @@
 from .agents import MusicCoordinatorAgent
-from .motif_reconcile import reconcile_repeated_sections_enhanced
-from . import pipeline_v2 as _pipeline_v2
-
-_pipeline_v2.reconcile_repeated_sections = reconcile_repeated_sections_enhanced
 
 __all__ = ["MusicCoordinatorAgent"]
