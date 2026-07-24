059d712cc0a7907d9ddfc322f718d0d99e10b576
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-24 17:15:29 +0900

    Install final Accuracy V2 quality extension

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,6 +1,8 @@
 from .agents import MusicCoordinatorAgent
 from .accuracy_v2_finalization import install_accuracy_v2_finalization
+from .quality_extension import install_quality_extension
 
 install_accuracy_v2_finalization()
+install_quality_extension()
 
 __all__ = ["MusicCoordinatorAgent"]
