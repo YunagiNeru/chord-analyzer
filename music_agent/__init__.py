3361ac57ad8e166bc69cf0336c99d7d7e573c628
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-24 17:02:43 +0900

    Install final Accuracy V2 safeguards

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,3 +1,6 @@
 from .agents import MusicCoordinatorAgent
+from .accuracy_v2_finalization import install_accuracy_v2_finalization
+
+install_accuracy_v2_finalization()
 
 __all__ = ["MusicCoordinatorAgent"]
