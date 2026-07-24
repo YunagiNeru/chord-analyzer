1af0551e765211273421e6753068656109205316
夕凪ねる/ yunagi neru <yuneru.sleeep@gmail.com>
2026-07-24 19:07:16 +0900

    Install final Accuracy V2 release recovery

--- music_agent/__init__.py
+++ music_agent/__init__.py
@@ -1,8 +1,10 @@
 from .agents import MusicCoordinatorAgent
 from .accuracy_v2_finalization import install_accuracy_v2_finalization
 from .quality_extension import install_quality_extension
+from .accuracy_v2_release_recovery import install_accuracy_v2_release_recovery
 
 install_accuracy_v2_finalization()
 install_quality_extension()
+install_accuracy_v2_release_recovery()
 
 __all__ = ["MusicCoordinatorAgent"]
